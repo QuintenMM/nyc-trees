e87b15561726c464f8853184f84d29eb5a9db919
QuintenMM <quinten.wildemeersch@gmail.com>
2021-07-01 14:30:04 +0200

    copy challenge trees

---
+++ challenge.py
@@ -0,0 +1,19 @@
+#TODO : Pull in the dataset
+#TODO : clean the dataset
+'''
+   - The dataset contains no missing values ("" or null)
+   - No duplicates.
+   - Values are consolidated
+   - Data format is correct
+   - No blank spaces (ex: " I love python " => "I love python")
+   -- sidetrack: The more rows of data you use, the better. However, pay attention that the more data you have, the longer each operation needs to execute.
+'''
+#TODO : extra checks
+'''
+- Can values be consolidated? (e.g. Truck and truck refer to the same thing)
+- Are there some columns where most of the data is missing ?
+- Can you fill in some missing values ?
+- Is the date format correct?
+- Are some values integers that should be float or vice-versa ? Change the dtype.
+'''
+#TODO  : convert to .csv
